165549add8f60181392942e6b266642bbcc3e8ba
Jeff Pignataro <jeffp2662@gmail.com>
2019-10-24 13:40:36 -0600

    Implementing update person logic

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
-function Post(url, method, data) {
-    return jQuery.ajax({
-        url: url,
-        type: method,
-        data: JSON.stringify(data),
-        dataType: "json",
-        contentType: "application/json; charset=utf-8"
-    });
+function Post(url, data) {
+    return apiCall(url, "POST", data);
+}
+function Put(url, data) {
+    return apiCall(url, "PUT", data);
 }
 
 function Get(url) {
@@ -17,6 +14,23 @@ function Get(url) {
     });
 }
 
+function apiCall(url, method, data){
+    return jQuery.ajax({
+        url: url,
+        type: method,
+        data: JSON.stringify(data),
+        dataType: "json",
+        contentType: "application/json; charset=utf-8"
+    });
+}
+
 function FormFieldValue(form, fieldName) {
     return form.context[fieldName].value;
+}
+
+$.urlParam = function (name) {
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)')
+                      .exec(window.location.search);
+
+    return (results !== null) ? results[1] || 0 : false;
 }
--- js/person.js
+++ js/person.js
@@ -2,14 +2,30 @@ $("#AddPerson").submit(function (event) {
     event.preventDefault();
 
     var form = $(this),
-        url = form.attr('action'),
-        method = form.attr('method');
+        url = form.attr('action');
     var data = {
         "name": FormFieldValue(form, 'inputName'),
         "age": FormFieldValue(form, 'inputAge')
     };
-    
-    Post(url, method, data).then(function (result) {
+
+    Post(url, data).then(function (result) {
+        $(".result").html(JSON.stringify(result));
+    });
+});
+
+$("#UpdatePerson").submit(function (event) {
+    event.preventDefault();
+
+    var form = $(this),
+        url = form.attr('action'),
+        method = form.attr('method');
+    var data = {
+        "id": FormFieldValue(form, 'hiddenId'),
+        "name": FormFieldValue(form, 'inputName'),
+        "age": FormFieldValue(form, 'inputAge')
+    };
+
+    Put(url, data).then(function (result) {
         $(".result").html(JSON.stringify(result));
     });
 });
